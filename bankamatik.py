ded58f0c104b4ba633c3f7620a4fb064a1aba58a
Emreokul <emre.tulgarlar@std.yeditepe.edu.tr>
2024-12-25 17:36:06 +0300

    bankamatik

---
+++ bankamatik.py
@@ -0,0 +1,11 @@
+bakiye=float(input("bakiye"))
+#cekilecek_miktar=float(input("cekilecek miktar"))
+#yatırılacak_miktar=float(input("yatırılacak miktar"))
+#guncel_bakiye=float(input("güncel miktar"))
+while True:
+  print("para cek")
+  print("para yatır")
+  break
+else:
+secim=input("o/1")
+
