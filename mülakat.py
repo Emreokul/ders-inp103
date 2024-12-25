b56f6638549ef1b3c02a1f6bde2d6c0e5aa6a420
Emreokul <emre.tulgarlar@std.yeditepe.edu.tr>
2024-12-25 16:57:44 +0300

    mülakat

---
+++ mülakat.py
@@ -0,0 +1,13 @@
+yas=float(input("YAŞ"))
+tecrube =float(input("tecrube"))
+
+if tecrube >4:
+  
+  if  20<=yas<=50:
+    print("reddedildi")
+elif tecrube>4:
+  print("mulakat")
+elif tecrube<64:
+    print("reddildi")
+else:
+  print("mülakat")
